93f6511626f6bd8876ce8fd4625b086688dcd41d
Robert Straw <drbawb@fatalsyntax.com>
2016-11-17 16:15:27 -0600

    (bump version)

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Popura.Mixfile do
 
   def project do
     [app: :popura,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
