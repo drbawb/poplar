3bbd6dc54e25b55c1c480e087a3a56871d20b813
Robert Straw <drbawb@fatalsyntax.com>
2016-11-17 16:02:52 -0600

    (deps) add csv parser

--- mix.exs
+++ mix.exs
@@ -39,7 +39,8 @@ defmodule Popura.Mixfile do
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
      {:httpoison, "~> 0.10"},
-     {:remote_ip, "~> 0.1"}]
+     {:remote_ip, "~> 0.1"},
+     {:csv, "~> 1.4"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
